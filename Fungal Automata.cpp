44164c96a7d4dcb833dd5530ed61fa0ea5a1cd52
Jonas Seiler <SirPiixel@gmail.com>
2020-05-29 21:15:29 +0200

    Beautified output

--- Fungal Automata.cpp
+++ Fungal Automata.cpp
@@ -38,11 +38,11 @@ void nextStep()
 
 int main()
 {
-    std::cout << "input lenght\n";
+    std::cout << "What is the length of the universe?\n";
     std::cin >> length;
     current = std::vector<int>(length, 0);
     future = std::vector<int>(length, 0);
-    std::cout << "input rule\n";
+    std::cout << "Which rule should be used?\n";
     std::cin >> rule;
     int ruleDiv = rule;
     int div = 128;
@@ -51,20 +51,23 @@ int main()
         ruleDiv = ruleDiv - rules[i] * div;
         div = div / 2;
     }
-    for (int ruletest = 0; ruletest < 8; ruletest++) {
-        std::cout << rules[ruletest];
+    std::cout << "[111|110|101|100|011|010|001|000]\n[";
+    for (int ruletest = 0; ruletest < 7; ruletest++) {
+        std::cout << " " << rules[ruletest] << " |";
     }
-    std::cout << "add ones\n";
+    std::cout << " " << rules[7] << " ]\n";
+    std::cout << "Which cells should be alive?\n";
     std::string input;
     std::cin >> input;
     while(input != "next") {
         current[std::stoi(input)] = 1;
         std::cin >> input;
     }
+    std::cout << "Current configuration:\n";
     for (int onetest = 0; onetest < length; onetest++) {
         std::cout << current[onetest];
     }
-    std::cout << "how many steps?\n";
+    std::cout << "\nHow many steps should be simulated?\n";
     int count;
     std::cin >> count;
     for (int j = 0; j < count; j++) {
