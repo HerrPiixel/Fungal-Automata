c0fba36c5c0bb906bea8120c7c5f54680f11a72e
Jonas Seiler <SirPiixel@gmail.com>
2020-05-29 21:02:26 +0200

    cleared useless comments

--- Fungal Automata.cpp
+++ Fungal Automata.cpp
@@ -1,5 +1,3 @@
-// Fungal Automata.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
 
 #include <iostream>
 #include <vector>
@@ -14,9 +12,6 @@ std::vector<int> rules(8, 0);
 void nextStep()
 {
     int rulecase = 0;
-    //for (int onetest2 = 0; onetest2 < length; onetest2++) {
-        //std::cout << current[onetest2];
-    //}
     for (int k = 0; k < length; k++) {
         if (k == 0) {
             rulecase = current[k] * 2 + current[k + 1];
@@ -24,7 +19,6 @@ void nextStep()
         }
         else if (k == length - 1) {
             rulecase = current[k - 1] * 4 + current[k] * 2;
-            //std::cout << "last cell:" << 7 - rulecase;
             future[k] = rules[7 - rulecase];
         }
         else {
@@ -76,17 +70,4 @@ int main()
     for (int j = 0; j < count; j++) {
         nextStep();
     }
-    //std::cout << char(219) << " " << char(219) << char(176) << char(178)  << "\n";
-    //std::cout << "  " << char(219) << "\n";
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
